bd40161c2f27d50a854741806328756341df1f6a
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-06-29 17:28:46 +0300

    Created InitialPage.jsx, added routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import {BrowserRouter, Route, Routes, Outlet} from "react-router-dom";
+import InitialPage from "./components/initialPage/InitialPage";
 import './App.css';
 
-function App() {
+const App = () => {
     return (
         <div className={"app"}>
-            app
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<Layout />}>
+                        <Route index element={<InitialPage />}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div>
+    )
+}
+
+const Layout = () => {
+    return (
+        <>
+            <Outlet/>
+        </>
     )
 }
 
---
+++ src/components/initialPage/InitialPage.jsx
@@ -0,0 +1,9 @@
+const InitialPage = () => {
+    return (
+        <main className={"initialPage"}>
+           <button>COUNTRIES</button>
+        </main>
+    )
+}
+
+export default InitialPage
