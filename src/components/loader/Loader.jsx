f08fa3ddf3fd0b57a074bf1bb96fc555e65462df
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-06-29 20:27:46 +0300

    Created Loader component

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import './Loader.sass'
+import loader from './loader.gif'
+
+const Loader = () => (
+    <div className="loader">
+        <img src={loader} alt="loader"/>
+    </div>
+)
+
+export default Loader
