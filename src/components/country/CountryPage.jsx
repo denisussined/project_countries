bdc08b613e2e20f7b1cf2c159449b972d95a38a3
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-06-30 15:27:26 +0300

    Fixed "Each child ... key prop" warning

--- src/components/country/CountryPage.jsx
+++ src/components/country/CountryPage.jsx
@@ -11,22 +11,18 @@ const CountryPage = ({desc, info}) => {
     )
 }
 
-const genTable = (desc, info) => { //return an array, like [<desc/> <info/> <desc/> <info/> ...]
+const genTable = (desc, info) => { //return an array, like [<desc/>, <info/>, <desc/>, <info/>, ...]
     let table = []
     for (let i = 0; i < desc.length; i++) {
         if (i === 0) {
             table.push(
-                <>
-                    <div key={i} className={"desc"}>{desc[i]}</div>
-                    <img key={i+desc.length} src={info[i]} className={"info flag"} alt={'flag'}/>
-                </>
+                <div key={i} className={"desc"}>{desc[i]}</div>,
+                <img key={i+desc.length} src={info[i]} className={"info flag"} alt={'flag'}/>
             )
         } else {
             table.push(
-                <>
-                    <div key={i} className={"desc"}>{desc[i]}</div>
-                    <div key={i+desc.length} className={"info"}>{info[i]}</div>
-                </>
+                <div key={i} className={"desc"}>{desc[i]}</div>,
+                <div key={i+desc.length} className={"info"}>{info[i]}</div>
             )
         }
     }
