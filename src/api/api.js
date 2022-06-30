54f2e965cc97b850206f7080bfa5a2ba0ba0c819
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-06-30 15:55:56 +0300

    Changed api.js and thunkCreators.js

--- src/api/api.js
+++ src/api/api.js
@@ -7,22 +7,5 @@ const instance = axios.create({
 export const api = {
     getCountries: () => instance.get('all'),
 
-    getCountry: name => instance.get(`name/${name}`).then(res => {
-        if (res.status === 200) {
-            return {
-                'flag': res.data[0].flags.svg,
-                'country': res.data[0].name.common,
-                'official name': res.data[0].name.official,
-                'capital': res.data[0].capital.join(' '),
-                'region': res.data[0].region,
-                'languages': res.data[0].languages ? Object.values(res.data[0].languages).join(', ') : '---',
-                'currencies': res.data[0].currencies ? Object.values(res.data[0].currencies).map(cur => cur.name).join(' ') : '---',
-                'area': res.data[0].area,
-                'population': res.data[0].population,
-                'borders': res.data[0].borders ? res.data[0].borders.join(', ') : '---',
-            }
-
-        } else console.log('api.getCountry ERROR')
-
-    })
+    getCountry: name => instance.get(`name/${name}`).then(res => res.data[0])
 }
--- src/redux/thunkCreators.js
+++ src/redux/thunkCreators.js
@@ -3,7 +3,20 @@ import {api} from "../api/api";
 
 export const getCountry = (name) => dispatch => {
 
-    api.getCountry(name).then(info => {
+    api.getCountry(name).then(data => {
+        let info = {
+            'flag': data.flags.svg,
+            'country': data.name.common,
+            'official name': data.name.official,
+            'capital': data.capital.join(' '),
+            'region': data.region,
+            'languages': data.languages ? Object.values(data.languages).join(', ') : '---',
+            'currencies': data.currencies ? Object.values(data.currencies).map(cur => cur.name).join(' ') : '---',
+            'area': data.area,
+            'population': data.population,
+            'borders': data.borders ? data.borders.join(', ') : '---',
+        }
+
         dispatch(setCountry(info))
     })
 }
